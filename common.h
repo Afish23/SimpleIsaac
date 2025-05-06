3173d634d6b77aca5adc359f75acabbe489e97da
Afish <2054641315@qq.com>
2025-05-06 14:52:13 +0800

    添加项目文件。

---
+++ common.h
@@ -0,0 +1,9 @@
+#pragma once
+#pragma comment(lib, "winmm.lib")
+#include <iostream>
+#include <stdlib.h>
+#include <easyx.h>
+#include <ctime>
+#include <cmath>
+#include <shlobj.h> 
+using namespace std;
